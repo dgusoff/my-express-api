0bb19bacf357408e9ccf27ffc3ce74ca54b3deb0
Derek Gusoff <dgusoff1@gmail.com>
2021-06-11 16:03:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+    try {
+        let data = await callGithub();
+        res.send(data)
+    }
+    catch (err) {
+        console.log(err);
+        res.send(err.message)
+    }
+});
+
+app.get('/time', async (req, res) => {
+    try {
+        let now = new Date();
+        res.send({
+            time: now.toDateString()
+        })
+    }
+    catch (err) {
+        console.log(err);
+        res.send(err.message)
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
+
+
+async function callGithub() {
+    const data = await axios.get('https://api.github.com/');
+    console.log(data.data);
+    return data.data;
+}
