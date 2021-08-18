65572465c0686d761c2c8e92157997efac690be9
Derek Gusoff <dgusoff1@gmail.com>
2021-08-18 16:26:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT;
 
 app.get('/', async (req, res) => {
     try {
